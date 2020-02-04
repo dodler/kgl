0fa60e0cb5cdd91f72c0c94d52b443a25dc8e0ad
lyan <tntlagf93@mail.ru>
2020-02-04 21:48:54 +0300

    experiments

---
+++ bangali_19/augmentations/compose/v1.py
@@ -0,0 +1,28 @@
+from albumentations import Compose, Resize, RandomCrop, Flip, HorizontalFlip, VerticalFlip, Transpose, RandomRotate90, \
+    ShiftScaleRotate, OneOf, Blur, MotionBlur, MedianBlur, GaussianBlur, RandomBrightnessContrast
+from albumentations.pytorch import ToTensor
+
+train_aug = Compose([
+    RandomCrop(height=96, width=96, p=0.4),
+    OneOf([
+        VerticalFlip(p=0.2),
+        HorizontalFlip(p=0.3),
+        Transpose(p=0.2),
+        RandomRotate90(p=0.2),
+    ], p=0.5),
+    ShiftScaleRotate(p=0.5),
+    OneOf([
+        Blur(p=0.2),
+        MotionBlur(p=0.2),
+        MedianBlur(p=0.2),
+        GaussianBlur(p=0.2),
+    ], p=0.4),
+    RandomBrightnessContrast(p=0.5, brightness_limit=0.4, contrast_limit=0.4),
+    Resize(128, 128, always_apply=True),
+    ToTensor()
+])
+
+valid_aug = Compose([
+    Resize(128, 128, always_apply=True),
+    ToTensor()
+])
