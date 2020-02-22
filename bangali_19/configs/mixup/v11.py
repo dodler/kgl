f37f4cb058a3fe46b09608e6f94a57f0a87bf724
lyan <tntlagf93@mail.ru>
2020-02-22 09:23:39 +0300

    experiments

---
+++ bangali_19/configs/mixup/v11.py
@@ -0,0 +1,20 @@
+config = {
+    "arch": "multi-head",
+    "backbone": "densenet121",
+    "pretrained": True,
+    "in-bn": False,
+    'opt': 'radam',
+    'loss_aggregate_fn': 'mean',
+    'schedule': 'cosine_annealing_warm_restarts',
+    'T_0': 6,
+    'lr': 1e-2,
+    'early_stop_epochs': 25,
+    'epochs': 150,
+    'train_aug': 'augmentations.v1',
+    'valid_aug': 'augmentations.v1',
+    'img_path': '/home/lyan/train/',
+    'mixup': True,
+    'mixup_alpha': 0.4,
+    'folds_path':'/home/lyan/Documents/kaggle/bangali_19/folds3.csv',
+}
+
