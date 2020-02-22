7192931f6caa8ea7999389cddd003374d62cc0e0
lyan <tntlagf93@mail.ru>
2020-02-22 09:23:25 +0300

    experiments

---
+++ bangali_19/configs/backbones/dnet_v1_heavy_aug_v5.py
@@ -0,0 +1,15 @@
+config = {
+    "arch": "multi-head",
+    "backbone": "densenet161",
+    "pretrained": True,
+    "in-bn": True,
+    'opt': 'radam',
+    'loss_aggregate_fn': 'mean',
+    'schedule': 'cosine_annealing_warm_restarts',
+    'T_0': 6,
+    'lr': 1e-3,
+    'train_aug': 'augmentations.compose.v0_heavy',
+    'valid_aug': 'augmentations.geom.v0',
+    'folds_path':'/home/lyan/Documents/kaggle/bangali_19/folds4.csv',
+    'img_path': '/home/lyan/train/',
+}
